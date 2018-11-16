d2579f6c45af0a841c4afdac2511b7a4d3e3de3e
Lance Hannestad <lhannest@sfu.ca>
2018-11-16 15:11:23 -0800

    Added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import kgx
+
+t = kgx.RdfOwlTransformer()
+t.parse('data/mondo.owl')
+
+t = kgx.ObanRdfTransformer(t)
+t.parse('data/orphanet.ttl')
+
+n = kgx.NeoTransformer(
+    host='localhost',
+    ports={'bolt' : '8087'},
+    username='neo4j',
+    password='password'
+)
+
+n.graph = t.graph
+n.save_with_unwind()
