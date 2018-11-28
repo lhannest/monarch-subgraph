cb332482280e25b4bc004b324308bce2b6a83265
Lance Hannestad <lhannest@sfu.ca>
2018-11-28 09:38:34 -0800

    Added rdf_to_json.py script

---
+++ rdf_to_json.py
@@ -0,0 +1,26 @@
+from kgx import ObanRdfTransformer, JsonTransformer
+
+t = ObanRdfTransformer()
+t.add_ontology('data/mondo.owl')
+t.parse('data/orphanet.ttl')
+t = JsonTransformer(t)
+t.save('orphanet.json')
+
+t = ObanRdfTransformer()
+t.add_ontology('data/hp.owl')
+t.parse('data/hpoa.ttl')
+t = JsonTransformer(t)
+t.save('hpoa.json')
+
+t = ObanRdfTransformer()
+# t.add_ontology('data/mondo.owl')
+t.parse('data/omim.ttl')
+t = JsonTransformer(t)
+t.save('omim.json')
+
+t = ObanRdfTransformer()
+t.add_ontology('data/go.owl')
+t.add_ontology('data/so.owl')
+t.parse('data/clinvar.ttl')
+t = JsonTransformer(t)
+t.save('clivar.json')
