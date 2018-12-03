a80f4ebd25aa392ad7a6421c20a2ecdeaf42435e
Lance Hannestad <lhannest@sfu.ca>
2018-12-03 08:10:08 -0800

    Added count script

---
+++ count.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from kgx import JsonTransformer
+from pprint import pprint
+
+t = JsonTransformer()
+t.parse('clinvar.json')
+categories = []
+for n in t.graph.nodes():
+    c = t.graph.node[n].get('category', None)
+    categories.append(c)
+
+d = Counter(category)
+
+pprint(d)
+
+with open('count.txt', 'w+') as f:
+    f.write(str(d))
