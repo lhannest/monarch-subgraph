5080a9121f91d844ed644d47f5b84db87ea56a0e
Lance Hannestad <lhannest@sfu.ca>
2019-01-10 16:04:35 -0800

    Moved HgncRdfTransformer into kgx

--- rdf_to_json.py
+++ rdf_to_json.py
@@ -1,4 +1,4 @@
-from kgx import ObanRdfTransformer, JsonTransformer
+from kgx import ObanRdfTransformer, JsonTransformer, HgncRdfTransformer
 from collections import Counter
 
 #o = ObanRdfTransformer()
@@ -9,28 +9,6 @@ from collections import Counter
 #o.add_ontology('data/ordo.owl')
 
 from rdflib import URIRef
-
-class HgncRdfTransformer():
-    def add_node_attribute(node:str, key:str, value:str) -> None:
-        if node in self.graph:
-            attr_dict = self.graph.node[node]
-            if key in attr_dict:
-                attr_dict[key].append(value)
-            else:
-                attr_dict[key] = [value]
-        else:
-            self.graph.add_node(node, **{key : [value]})
-
-    def load_edges(self, rdfgraph:rdflib.Graph, provided_by:str=None):
-        for s, p, o in rdfgraph.triples((None, None, None)):
-            s, p, o = str(s), str(p), str(o)
-
-            if p == 'http://purl.obolibrary.org/obo/IAO_0000136':
-                self.add_node_attribute(o, 'publications', s)
-            elif p == 'http://purl.obolibrary.org/obo/RO_0002524':
-                self.graph.add_edge(s, o, predicate='has_subsequence')
-            else p == 'http://purl.obolibrary.org/obo/RO_0002525':
-                self.graph.add_edge(o, s, predicate='has_subsequence')
 
 t = HgncRdfTransformer()
 t.parse('data/hgnc.ttl')
