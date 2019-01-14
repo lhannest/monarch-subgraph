6410347a69dcdc5df88c21f622921764f97ed0d1
Lance Hannestad <lhannest@sfu.ca>
2019-01-14 09:38:52 -0800

    Refactored whole parsing and clique merging workflow into main.py

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-import kgx
+from kgx import ObanRdfTransformer, JsonTransformer, HgncRdfTransformer
+from kgx import clique_merge
 
-t = kgx.RdfOwlTransformer()
-t.parse('data/mondo.owl')
+t = HgncRdfTransformer()
+t.parse('data/hgnc.ttl')
+t = JsonTransformer(t)
+t.save('hgnc.json')
 
-t = kgx.ObanRdfTransformer(t.graph)
+t = ObanRdfTransformer()
+t.add_ontology('data/hp.owl')
 t.parse('data/orphanet.ttl')
+t = JsonTransformer(t)
+t.save('orphanet.json')
 
-n = kgx.NeoTransformer(
-    host='localhost',
-    ports={'bolt' : '8087'},
-    username='neo4j',
-    password='password'
-)
+t = ObanRdfTransformer()
+t.ontologies = o.ontologies
+t.parse('data/hpoa.ttl')
+t = JsonTransformer(t)
+t.save('hpoa.json')
 
-n.graph = t.graph
-n.save_with_unwind()
+t = ObanRdfTransformer()
+t.add_ontology('data/mondo.owl')
+t.parse('data/omim.ttl')
+t = JsonTransformer(t)
+t.save('omim.json')
+
+t = ObanRdfTransformer()
+t.add_ontology('data/go.owl')
+t.add_ontology('data/so.owl')
+t.parse('data/clinvar.ttl')
+t = JsonTransformer(t)
+t.save('clinvar.json')
+
+t = JsonTransformer()
+t.parse('clinvar.json')
+t.parse('omim.json')
+t.parse('hpoa.json')
+t.parse('orphanet.json')
+t.save('merged.json')
+
+t.graph = clique_merge(t.graph)
+t.save('clique_merged.json')
